09338a4fd9a2c264075f92c46a306b1f74f4dd5c
AMAN SINGH <2amansingh07@gmail.com>
2026-09-25 15:31:16 +0530

    fix:error while importing prisma inside server

---
+++ apps/server/api/v1/auth/controller.ts
@@ -0,0 +1,15 @@
+
+import { Router } from "express";
+import { prisma } from "@repo/database";
+
+
+const router: Router = Router()
+
+router.post("/signup", (req, res) => {
+
+})
+
+
+router.post("/signin", (req, res) => {
+
+})
